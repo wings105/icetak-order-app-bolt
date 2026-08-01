276576928f50fb592990f7f284a10f127b687fd6
wings105 <msg.ebye@gmail.com>
2026-08-01 19:25:08 +0800

    Updated index.html

--- supabase/migrations/20260801183600_lock_admin_whatsapp_order_rpcs.sql
+++
@@ -1,6 +0,0 @@
-revoke execute on function public.icetak_admin_customer_lookup(text) from anon;
-revoke execute on function public.icetak_admin_create_whatsapp_paid_order(jsonb) from anon;
-revoke execute on function public.icetak_admin_order_sync_status(uuid) from anon;
-grant execute on function public.icetak_admin_customer_lookup(text) to authenticated,service_role;
-grant execute on function public.icetak_admin_create_whatsapp_paid_order(jsonb) to authenticated,service_role;
-grant execute on function public.icetak_admin_order_sync_status(uuid) to authenticated,service_role;
