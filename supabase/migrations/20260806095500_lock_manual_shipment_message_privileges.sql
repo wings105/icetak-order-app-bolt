294c832b77e15e8cecb0b7eab6bc6dd8fcfbd249
wings105 <msg.ebye@gmail.com>
2026-08-06 09:50:23 +0800

    security: restrict manual shipment message table privileges

---
+++ supabase/migrations/20260806095500_lock_manual_shipment_message_privileges.sql
@@ -0,0 +1,3 @@
+revoke all on table public.shipment_message_jobs from authenticated;
+grant select, update on table public.shipment_message_jobs to authenticated;
+revoke all on table public.shipment_message_jobs from anon;
